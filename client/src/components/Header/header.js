fe78514d54730d557ae606925b465fff64ef9998
Clark Alms <clark.alms@gmail.com>
2020-03-26 23:30:58 -0500

    added basic routing and got navbar functionality working

---
+++ client/src/components/Header/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import HeaderImg from '../../images/backgroundImg.jpg';
+import './header.css'
+
+class header extends Component {
+    render() {
+        return (
+            <div className="header-wrapper">
+                <div className="header">
+                    <h1>ALEXANDER CLARK ALMS</h1>
+                </div>
+               
+            </div>
+        );
+    }
+}
+
+export default header;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from '../components/Header/header';
+class Home extends Component {
+    render() {
+        return (
+            <div className="home-page-wrapper">
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default Home;
